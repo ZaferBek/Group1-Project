c9595d7da69b83921387107ff5d45f9bd6a8980e
Tyson9 <tahsinkodat@gmail.com>
2020-10-13 19:17:05 +0100

    new changes

--- src/test/java/vytrackLogin.java
+++ src/test/java/vytrackLogin.java
@@ -3,6 +3,10 @@ public class vytrackLogin {
     public static void main(String[] args) {
 
         System.out.println("Login the page");
+
+        System.out.println("Enter password");
+
+        System.out.println("Bla bla bla...");
     }
 }
 
