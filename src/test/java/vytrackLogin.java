4bef0d6bada554167a650eda3d689a17e59facb1
muhsintamturk <65741791+muhsintamturk@users.noreply.github.com>
2020-10-13 20:24:14 +0100

    Update vytrackLogin.java

--- src/test/java/vytrackLogin.java
+++ src/test/java/vytrackLogin.java
@@ -3,6 +3,8 @@ public class vytrackLogin {
     public static void main(String[] args) {
 
         System.out.println("Login the page");
+        
+        // "O my Lord, increase us in knowledge"
     }
 }
 
